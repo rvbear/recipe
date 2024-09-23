7f6b3791ff0bcdacd81b6bfdeb03f368e9aed1ee
rvbear <rvbear2000@gmail.com>
2024-09-23 23:06:27 +0900

    chore: init Setting

---
+++ src/test/java/com/potato/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potato.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
