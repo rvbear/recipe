9e3fa01eceda3ec08025b5596f76beae7266af80
rvbear <rvbear2000@gmail.com>
2024-10-18 22:38:45 +0900

    feat: add annotation for test

--- src/main/java/com/potato/recipe/user/domain/dto/RequestLoginDTO.java
+++ src/main/java/com/potato/recipe/user/domain/dto/RequestLoginDTO.java
@@ -1,8 +1,10 @@
 package com.potato.recipe.user.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RequestLoginDTO {
     String userName;
     String password;
--- src/main/java/com/potato/recipe/user/domain/dto/RequestSignUpDTO.java
+++ src/main/java/com/potato/recipe/user/domain/dto/RequestSignUpDTO.java
@@ -1,8 +1,10 @@
 package com.potato.recipe.user.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RequestSignUpDTO {
     String userName;
     String password;
